204d3d49003de48776467512d4207c34e33573d1
Jeferson <jeferson.silva@yara.com>
2023-06-30 22:34:31 -0300

    fix(balance): updating transaction usage

--- src/services/balance.js
+++ src/services/balance.js
@@ -11,41 +11,42 @@ const makeDeposit = async (req, res) =>{
   const { Job, Contract, Profile } = req.app.get('models')
   const { userId } = req.params
   const { deposit } = req.body
-
+fix(balance): updating transaction usage
   if(!deposit) return res.status(403).json({ message: 'Error: A deposit value is required' }).end()
-
-  const profile = await Profile.findOne({
-      where: {
-          id: userId,
-         type: ProfileTypes.CLIENT
-      },
-      include: [{
-          model: Contract,
-          as: 'Client',
-          where: {
-            status: {
-              [Op.or]: [ContractStatuses.NEW, ContractStatuses.INPROGRESS]
-            },
-          },
-          include: [{            
-              model: Job,
-              where: {
-                paid: null,
-              }
-          }]
-      }],
-  })
-
-  if(!profile?.Client || profile?.Client.length === 0) return res.status(404).json({ message: 'Error: Job or contract not found' }).end()
-  const totalPerJobsFunc = (initialValue, job) => initialValue + job.price
-  const totalPerContractFunc = (initialClientValue, client) => initialClientValue + client.Jobs.reduce(totalPerJobsFunc, 0)
-  const clientTotalJobs = profile.Client.reduce(totalPerContractFunc, 0)
-  const MAXDEPOSIT = (clientTotalJobs*25)/100
-
-  if(deposit > MAXDEPOSIT) return res.status(403).json({ message: `Error: Max deposit permited: ${MAXDEPOSIT}` }).end()
 
   const t = await sequelize.transaction();
   try {
+      const profile = await Profile.findOne({
+          where: {
+              id: userId,
+             type: ProfileTypes.CLIENT
+          },
+          include: [{
+              model: Contract,
+              as: 'Client',
+              where: {
+                status: {
+                  [Op.or]: [ContractStatuses.NEW, ContractStatuses.INPROGRESS]
+                },
+              },
+              include: [{            
+                  model: Job,
+                  where: {
+                    paid: null,
+                  }
+              }]
+          }],
+      }, transaction: t)
+    
+      if(!profile?.Client || profile?.Client.length === 0) return res.status(404).json({ message: 'Error: Job or contract not found' }).end()
+      const totalPerJobsFunc = (initialValue, job) => initialValue + job.price
+      const totalPerContractFunc = (initialClientValue, client) => initialClientValue + client.Jobs.reduce(totalPerJobsFunc, 0)
+      const clientTotalJobs = profile.Client.reduce(totalPerContractFunc, 0)
+      const MAXDEPOSIT = (clientTotalJobs*25)/100
+
+      if(deposit > MAXDEPOSIT) return res.status(403).json({ message: `Error: Max deposit permited: ${MAXDEPOSIT}` }).end()
+
+  
       await profile.increment('balance', { by: deposit, where: { id: userId }, transaction: t})
       await t.commit();
   } catch (error) {
